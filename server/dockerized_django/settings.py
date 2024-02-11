044c1983152ed62f740295ca214eed555827c856
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-02-11 21:29:26 +0330

    feat: load the allowed hosts from environment variables in django settings file

--- server/dockerized_django/settings.py
+++ server/dockerized_django/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 INSTALLED_APPS = [
     'django.contrib.admin',
