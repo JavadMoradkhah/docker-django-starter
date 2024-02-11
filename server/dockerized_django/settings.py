2d07e6898ef0da395eebfd1bddfa0c6f29a20cd9
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-02-11 21:36:13 +0330

    feat: set Django debug to false by default

--- server/dockerized_django/settings.py
+++ server/dockerized_django/settings.py
@@ -18,7 +18,7 @@ def get_secret(key, default=None):
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
