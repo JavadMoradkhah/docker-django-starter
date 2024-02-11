c1bc4fbb21a12461e38cc5de0aadb8b1419bf991
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-02-11 21:40:12 +0330

    feat: add comment to the allowed hosts in the settings file

--- server/dockerized_django/settings.py
+++ server/dockerized_django/settings.py
@@ -20,6 +20,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
+# 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
+# For example: DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 INSTALLED_APPS = [
