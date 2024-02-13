93c7c1cbcf14b6293db0e0ba794ecbe201b672da
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-02-13 19:55:05 +0330

    feat: build view, template, and form to test uploading files

--- server/core/views.py
+++ server/core/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
+from .forms import UploadFileForm
 
 
 def home(request):
     return HttpResponse('Home Page')
+
+
+def upload_file(request):
+    form = UploadFileForm()
+
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            file = request.FILES["file"]
+            storage = FileSystemStorage()
+            storage.save(file.name, file)
+            return HttpResponse("File uploaded successfully")
+
+    return render(request, 'core/index.html', {'form': form})
