af4a1565fd2c1523fdad386384201da967d7a723
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-02-28 19:39:07 +0330

    refactor: remove the "STATICFILES_DIRS" setting, since we don't need it

--- server/dockerized_django/settings/common.py
+++ server/dockerized_django/settings/common.py
@@ -85,10 +85,6 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
-
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = BASE_DIR / "mediafiles"
